44f1cb74b82480e139b6bccb779247b352c3c83e
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-10-18 20:28:09 +0200

    Excluded packages list sorted

--- cleaner/src/main/kotlin/pl/droidsonroids/stf/devicecleaner/ExcludedPackages.kt
+++ cleaner/src/main/kotlin/pl/droidsonroids/stf/devicecleaner/ExcludedPackages.kt
@@ -1,18 +1,17 @@
 package pl.droidsonroids.stf.devicecleaner
 
 val rawExcludedPackages = """
-jp.co.cyberagent.stf
-de.codenauts.hockeyapp
-pl.droidsonroids.devico
-com.ghisler.android.TotalCommander
 com.android.chrome
 com.facebook.katana
-pl.droidsonroids.devico
-com.instagram.android
 com.facebook.orca
+com.ghisler.android.TotalCommander
+com.google.android.apps.plus
+com.google.android.instantapps.supervisor
+com.instagram.android
 com.koushikdutta.vysor
 com.lexa.fakegps
 com.twitter.android
-com.google.android.apps.plus
-com.google.android.instantapps.supervisor
+de.codenauts.hockeyapp
+jp.co.cyberagent.stf
+pl.droidsonroids.devico
 """
